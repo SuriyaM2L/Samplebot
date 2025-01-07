2e1cae13b9be52ccad1b21de7b9b7b720d7b4abc
Suriya <76084500+SuriyaM2L@users.noreply.github.com>
2025-01-07 19:17:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ API_ID = "7851526"
 API_HASH = "93ba4db0ad662e558356871afe8ca6de"
 BOT_TOKEN = "7737992517:AAHVqoKAEmn8dZzPfD-3hjOKMdNCT4wvLuk"
 
-CodeRed = Client(
+TechMagazine = Client(
     name="SampleBot",
     api_id=API_ID,
     api-hash=API_HASH,
